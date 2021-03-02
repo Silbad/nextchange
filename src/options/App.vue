0d7a12c78e4de59b7e6cc56501bd28de9ff81c1c
silbad <psilbad@gmail.com>
2021-03-02 20:03:24 +0100

    Update UI for options page (Bootstrap)

--- src/options/App.vue
+++ src/options/App.vue
@@ -1,13 +1,68 @@
 <template>
-<div>
-    test
-</div>
+    <b-container fluid>
+        <b-alert class="mt-4" show>{{ defaultText }}</b-alert>
+
+        <b-form-group
+            id="fieldset-1"
+            description="Let us know your name."
+            label="Enter your name"
+            label-for="input-1"
+            valid-feedback="Thank you!"
+        >
+            <b-form-input
+                id="input-1"
+                v-model="name"
+                :state="state"
+                trim
+            ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+            id="fieldset-1"
+            description="Let us know your name."
+            label="Enter your name"
+            label-for="input-1"
+            valid-feedback="Thank you!"
+        >
+            <b-form-input
+                id="input-1"
+                v-model="name"
+                :state="state"
+                trim
+            ></b-form-input>
+        </b-form-group>
+
+        <b-form-group
+            id="fieldset-1"
+            description="Let us know your name."
+            label="Enter your name"
+            label-for="input-1"
+            valid-feedback="Thank you!"
+        >
+            <b-form-input
+                id="input-1"
+                v-model="name"
+                :state="state"
+                trim
+            ></b-form-input>
+        </b-form-group>
+
+        <b-button variant="primary" class="mb-4">Valider</b-button>
+    </b-container>
 </template>
 
 <script>
-
 export default {
     name: "Options",
-    components: { },
+    components: {},
+    computed: {
+        defaultText() {
+            return browser.i18n.getMessage("extName");
+        },
+    },
 };
 </script>
+
+<style lang="scss">
+@import "@/assets/scss/options.scss";
+</style>
